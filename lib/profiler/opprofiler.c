8ef23dce6174699c81de9b4870385169f1d4b0d0
Nelly Kuznetsova <nelly.smitt@gmail.com>
2019-11-24 19:18:20 +0100

    Override syscalls in shared lib
    Overrides following syscalls in shared lib:
    malloc, calloc, realloc, free, open, read, write, close

--- lib/profiler/opprofiler.c
+++ lib/profiler/opprofiler.c
@@ -1,2 +1,72 @@
-#include <stdio.h>
 #include "opprofiler.h"
+
+void *__malloc(size_t size){
+	return ((__malloc_func) dlsym(RTLD_NEXT, "malloc"))(size);
+}
+
+void *malloc(size_t size){
+	return __malloc(size);
+}
+
+void __free(void *ptr){
+	((__free_func) dlsym(RTLD_NEXT, "free"))(ptr);
+}
+
+void free(void *ptr){
+	__free(ptr);
+}
+
+void *__calloc(size_t nmemb, size_t size){
+	return ((__calloc_func) dlsym(RTLD_NEXT, "calloc"))(nmemb, size);
+}
+
+void *calloc(size_t nmemb, size_t size){
+	return __calloc(nmemb, size);
+}
+
+void *__realloc(void *ptr, size_t size){
+	return ((__realloc_func) dlsym(RTLD_NEXT, "realloc"))(ptr, size);
+}
+
+void *realloc(void *ptr, size_t size){
+	return __realloc(ptr, size);
+}
+
+int __open(const char *pathname, int flags, mode_t mode){
+	return ((__open_func) dlsym(RTLD_NEXT, "open"))(pathname, flags, mode);
+}
+
+int open(const char *pathname, int flags, ...){
+	int mode = 0;
+	if (__OPEN_NEEDS_MODE (flags)){
+                va_list arg;
+                va_start (arg, flags);
+                mode = va_arg (arg, int);
+                va_end (arg);
+        }
+	return __open(pathname, flags, mode);
+}
+
+int __close(int fd){
+	return ((__close_func)dlsym(RTLD_NEXT, "close"))(fd);
+}
+
+int close(int fd){
+	return __close(fd);
+}
+
+ssize_t __read(int fd, void *buf, size_t count){
+	return ((__read_func)dlsym(RTLD_NEXT, "read"))(fd, buf, count);
+}
+
+ssize_t read(int fd, void *buf, size_t count){
+	return __read(fd, buf, count);
+}
+
+ssize_t __write(int fd, const void *buf, size_t count){
+	return ((__write_func)dlsym(RTLD_NEXT, "write"))(fd, buf, count);
+}
+
+ssize_t write(int fd, const void *buf, size_t count){
+	return __write(fd, buf, count);
+}
--- lib/profiler/opprofiler.h
+++ lib/profiler/opprofiler.h
@@ -1,4 +1,21 @@
 #ifndef OP_PROFILER
 #define OP_PROFILER
 
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <stdarg.h>
+#include <dlfcn.h>
+#include <unistd.h>
+
+typedef void * (*__malloc_func)(size_t size);
+typedef void (*__free_func)(void *ptr);
+typedef void * (*__calloc_func)(size_t nmemb, size_t size);
+typedef void * (*__realloc_func)(void *ptr, size_t size);
+typedef int (*__open_func)(const char *pathname, int flags, mode_t mode);
+typedef int (*__close_func)(int fd);
+typedef ssize_t (*__read_func)(int fd, void *buf, size_t count);
+typedef ssize_t (*__write_func)(int fd, const void *buf, size_t count);
+
 #endif
