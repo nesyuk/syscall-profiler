cd996542a4cfafce46d3ac25d42497d1b8d785fb
Nelly Kuznetsova <nelly.smitt@gmail.com>
2019-12-07 16:38:06 +0100

    Add new log file for logging events
    New log file for logging syscalls

--- lib/profiler/logger.c
+++ lib/profiler/logger.c
@@ -1,17 +1,24 @@
 #include "logger.h"
 
+static char path[126];
+
+void __log(const char *path, const char *msg){
+	int fd = __open(path, O_RDWR|O_CREAT|O_APPEND, FILE_PRIV);
+	__write(fd, msg, strlen(msg) + 1);
+	__close(fd);
+}
+
 void log_stats(const char *msg){
-	char path[126];
         sprintf(path, "./%s/%s", LOGS_DIR, STATS_FILE);
-        int fd = __open(path, O_RDWR|O_CREAT|O_APPEND, FILE_PRIV);
-        __write(fd, msg, strlen(msg) + 1);
-        __close(fd);
+        __log(path, msg);
 }
 
 void log_error(const char *msg){
-	char path[126];
         sprintf(path, "./%s/%s", LOGS_DIR, ERROR_FILE);
-        int fd = __open(path, O_RDWR|O_CREAT|O_APPEND, FILE_PRIV);
-        __write(fd, msg, strlen(msg) + 1);
-        __close(fd);
+        __log(path, msg);
+}
+
+void log_msg(const char *msg){
+	sprintf(path, "./%s/%s", LOGS_DIR, LOG_FILE);
+        __log(path, msg);
 }
--- lib/profiler/logger.h
+++ lib/profiler/logger.h
@@ -9,9 +9,11 @@
 #define LOGS_DIR "logs"
 #define STATS_FILE "stats_log.log"
 #define ERROR_FILE "errors.log"
+#define LOG_FILE "oplog.log"
 #define FILE_PRIV 0667
 
 void log_stats(const char *msg);
 void log_error(const char *msg);
+void log_msg(const char *msg);
 
 #endif
