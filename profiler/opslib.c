ef7fa2a4948c0ffe2b0ce179b30f09643a9adb10
Nelly Kuznetsova <nelly.smitt@gmail.com>
2019-12-15 14:16:27 +0100

    Move type definitions to header file
    Move syscall func type definitions to header file: opslib.h

--- profiler/opslib.c
+++ profiler/opslib.c
@@ -1,14 +1,5 @@
 #include "opslib.h"
 
-typedef void *(*__malloc_func)(size_t size);
-typedef void (*__free_func)(void *ptr);
-typedef void *(*__calloc_func)(size_t nmemb, size_t size);
-typedef void *(*__realloc_func)(void *ptr, size_t size);
-typedef int (*__open_func)(const char *pathname, int flags, mode_t mode);
-typedef int (*__close_func)(int fd);
-typedef ssize_t (*__read_func)(int fd, void *buf, size_t count);
-typedef ssize_t (*__write_func)(int fd, const void *buf, size_t count);
-
 void *__malloc(size_t size) {
   return ((__malloc_func)dlsym(RTLD_NEXT, "malloc"))(size);
 }
--- profiler/opslib.h
+++ profiler/opslib.h
@@ -6,6 +6,15 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+typedef void *(*__malloc_func)(size_t size);
+typedef void (*__free_func)(void *ptr);
+typedef void *(*__calloc_func)(size_t nmemb, size_t size);
+typedef void *(*__realloc_func)(void *ptr, size_t size);
+typedef int (*__open_func)(const char *pathname, int flags, mode_t mode);
+typedef int (*__close_func)(int fd);
+typedef ssize_t (*__read_func)(int fd, void *buf, size_t count);
+typedef ssize_t (*__write_func)(int fd, const void *buf, size_t count);
+
 void *__malloc(size_t size);
 void __free(void *ptr);
 void *__calloc(size_t nmemb, size_t size);
